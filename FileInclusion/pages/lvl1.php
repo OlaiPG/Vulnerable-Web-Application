354a3e571a0b75423c6ec622ded881b01269416a
OlaiPG <olaipg46@educastur.es>
2024-12-16 17:49:29 +0200

    Update lvl1.php

--- FileInclusion/pages/lvl1.php
+++ FileInclusion/pages/lvl1.php
@@ -7,24 +7,32 @@
 
    <body>    
       <div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='fileinc.html';">Main Page</button>  
+         <button type="button" name="homeButton" onclick="location.href='../../homepage.html';">Home Page</button>
+         <button type="button" name="mainButton" onclick="location.href='fileinc.html';">Main Page</button>  
       </div>
       
       <div align="center"><b><h3>This is Level 1</h3></b></div>
       <div align="center">
-      <a href=lvl1.php?file=1.php><button>Button</button></a>
-      <a href=lvl1.php?file=2.php><button>The Other Button!</button></a>
+         <a href="lvl1.php?file=1.php"><button>Button</button></a>
+         <a href="lvl1.php?file=2.php"><button>The Other Button!</button></a>
       </div>
       
       <?php
-        echo "</br></br>";
-        
-        if (isset( $_GET[ 'file' ]))        
-        {
-          @include($_GET[ 'file' ]);
-          echo"<div align='center'><b><h5>".$_GET[ 'file' ]."</h5></b></div> ";       
-        }
+         echo "</br></br>";
+         
+         if (isset($_GET['file'])) {
+            // Crear un conjunto de archivos permitidos
+            $allowed_files = array('1.php', '2.php');
+
+            // Obtener la entrada del usuario y validarla
+            $file = $_GET['file'];
+            if (in_array($file, $allowed_files)) {
+               @include($file);
+               echo "<div align='center'><b><h5>" . htmlspecialchars($file) . "</h5></b></div>";
+            } else {
+               echo "<div align='center'><b><h5>Archivo no permitido</h5></b></div>";
+            }
+         }
       ?>
    </body>
 </html>
