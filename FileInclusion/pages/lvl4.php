f7e3917b8227d7343a3d0b5796f15ab2f91e83e6
OlaiPG <olaipg46@educastur.es>
2024-12-16 17:55:42 +0200

    Update lvl4.php

--- FileInclusion/pages/lvl4.php
+++ FileInclusion/pages/lvl4.php
@@ -6,35 +6,33 @@
    </head>
 
    <body>
-      
       <div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='fileinc.html';">Main Page</button>      
+         <button type="button" name="homeButton" onclick="location.href='../../homepage.html';">Home Page</button>
+         <button type="button" name="mainButton" onclick="location.href='fileinc.html';">Main Page</button>      
       </div>
       
       <div align="center"><b><h3>This is Level 4</h3></b></div>
       <div align="center">
-      <a href=lvl4.php?file=1.php><button>Button</button></a>
-      <a href=lvl4.php?file=2.php><button>The Other Button!</button></a>
+         <a href="lvl4.php?file=1"><button>Button</button></a>
+         <a href="lvl4.php?file=2"><button>The Other Button!</button></a>
       </div>
       
       <?php     
-        echo "</br></br>";
+         echo "</br></br>";
 
-        if (isset( $_GET[ 'file' ])) 
-        {
-          $secure4 = $_GET[ 'file' ];
-         
-            if ($secure4!="1.php" && $secure4!="2.php") 
-            {
-              $secure4=substr($secure4, 0,-4);
+         if (isset($_GET['file'])) {
+            // Crear un conjunto de archivos permitidos
+            $allowed_files = array('1', '2');
+
+            // Obtener la entrada del usuario y validarla
+            $file = $_GET['file'];
+            if (in_array($file, $allowed_files)) {
+               include "$file.php";
+               echo "<div align='center'><b><h5>" . htmlspecialchars($file) . ".php</h5></b></div>";
+            } else {
+               echo "<div align='center'><b><h5>Archivo no permitido</h5></b></div>";
             }
-            
-            if (isset($secure4)) 
-            {        
-              include($secure4);              
-            }
-        }              
+         }
       ?>
    </body>
 </html>
