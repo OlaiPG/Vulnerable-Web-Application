f80de86d6d29e8932bc78723137600714d595646
OlaiPG <olaipg46@educastur.es>
2024-12-16 18:37:19 +0200

    Update XSS_level5.php

--- XSS/XSS_level5.php
+++ XSS/XSS_level5.php
@@ -2,27 +2,29 @@
 <html>
 <head>
     <title>XSS 5</title>
-<link rel="shortcut icon" href="../Resources/hmbct.png" />
+    <link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
     
-	 <div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='xssmainpage.html';">Main Page</button>
-    </div>
+<div style="background-color:#c9c9c9;padding:15px;">
+    <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+    <button type="button" name="mainButton" onclick="location.href='xssmainpage.html';">Main Page</button>
+</div>
 <div align="center">
-<form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="form">
-   <p>Your name:<input type="text" name="username"></p>
-   <input type="submit" name="submit" value="Submit">
+<form method="GET" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" name="form">
+    <p>Your name: <input type="text" name="username"></p>
+    <input type="submit" name="submit" value="Submit">
 </form>
     </div>
 
 <?php 
 if (isset($_GET["username"])) {
-    $user = str_replace("<", "", $_GET["username"]);
-    echo "Your name is "."$user";
+    // Sanitizar y escapar la entrada del usuario
+    $user = htmlspecialchars($_GET["username"], ENT_QUOTES, 'UTF-8');
+    echo "Your name is " . $user;
 }
  ?>
 
 </body>
 </html>
+
