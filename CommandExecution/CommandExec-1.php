0f8b041bcfcea106b13dca387e7a9627261d0466
OlaiPG <olaipg46@educastur.es>
2024-12-16 17:40:58 +0200

    Update CommandExec-1.php

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -10,7 +10,7 @@
     </div>
     <div style="background-color:#c9c9c9;padding:20px;">
       <h1 align="center">Login as Admin</h1>
-    <form align="center" action="CommandExec-1.php" method="$_GET">
+    <form align="center" action="CommandExec-1.php" method="GET">
       <label align="center">Username:</label><br>
       <input align="center" type="text" name="username" value="Admin"><br>
       <label>Password:</label><br>
@@ -21,12 +21,15 @@
   </div>
   <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
     <?php
-    if(isset($_GET["username"])){
-      echo shell_exec($_GET["username"]);
-      if($_GET["username"] == "Admin" && $_GET["password"] == "ufoundmypassword")
-        echo "WELLDONE";
+    if (isset($_GET["username"])) {
+        $username = escapeshellarg($_GET["username"]);
+        $password = $_GET["password"];
+        if ($username == "'Admin'" && $password == "ufoundmypassword") {
+            echo "WELLDONE";
+        } else {
+            echo "Invalid credentials.";
+        }
     }
-
     ?>
   </div>
   </body>
